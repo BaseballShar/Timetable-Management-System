1ccdb4eb0edcb135cffd5e0f85c0e7f103411ce6
Chan Tsz Ho (Baseball) <78458855+BaseballShar@users.noreply.github.com>
2022-11-11 11:49:30 +0800

    Add files via upload

--- facerecognition.sql
+++ facerecognition.sql
@@ -25,27 +25,99 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `Student`
 --
+
+DROP TABLE IF EXISTS `ClassTime`;
+DROP TABLE IF EXISTS `CourseNote`;
+DROP TABLE IF EXISTS `Enrol`;
+DROP TABLE IF EXISTS `TakePlace`;
 DROP TABLE IF EXISTS `Student`;
+DROP TABLE IF EXISTS `Course`;
+DROP TABLE IF EXISTS `Classroom`;
 
 # Create TABLE 'Student'
 CREATE TABLE `Student` (
-  `student_id` int NOT NULL,
+  `student_id` int NOT NULL PRIMARY KEY,
   `name` varchar(50) NOT NULL,
   `login_time` time NOT NULL,
-  `login_date` date NOT NULL
+  `login_date` date NOT NULL,
+  `email_address` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 LOCK TABLES `Student` WRITE;
 /*!40000 ALTER TABLE `Student` DISABLE KEYS */;
-INSERT INTO `Student` VALUES (1, "JACK", NOW(), '2021-01-20');
+INSERT INTO `Student` VALUES (1, "JACK", NOW(), '2021-01-20', "shar@sharmail.sharcom.shar");
 /*!40000 ALTER TABLE `Student` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
 # Create TABLE 'Course'
-# Create TABLE 'Classroom'
-# Create other TABLE...
+CREATE TABLE `Course` (
+  `course_id` varchar(25) NOT NULL PRIMARY KEY,
+  `zoom_link` varchar(100) NOT NULL,
+  `teacher_msg` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+LOCK TABLES `Course` WRITE;
+INSERT INTO `Course` VALUES ("COMP3278", "hi.zoom", "Please finish your asm");
+UNLOCK TABLES;
+
+# Create TABLE 'Classroom'
+CREATE TABLE `Classroom` (
+  `room_id` varchar(25) NOT NULL PRIMARY KEY,
+  `address` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `Classroom` WRITE;
+INSERT INTO `Classroom` VALUES ("MTW2", "Ming Wah Complex 2");
+UNLOCK TABLES;
+
+CREATE TABLE `Enrol` (
+  `student_id` int NOT NULL,
+  `course_id` varchar(25) NOT NULL,
+  PRIMARY KEY (student_id, course_id),
+  FOREIGN KEY (student_id) REFERENCES Student (student_id),
+  FOREIGN KEY (course_id) REFERENCES Course (course_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `Enrol` WRITE;
+INSERT INTO `Enrol` VALUES (1, "COMP3278");
+UNLOCK TABLES;
+
+CREATE TABLE `TakePlace` (
+  `room_id` varchar(25) NOT NULL,
+  `course_id` varchar(25) NOT NULL,
+  PRIMARY KEY (room_id, course_id),
+  FOREIGN KEY (room_id) REFERENCES Classroom (room_id),
+  FOREIGN KEY (course_id) REFERENCES Course (course_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `TakePlace` WRITE;
+INSERT INTO `TakePlace` VALUES ("MTW2", "COMP3278");
+UNLOCK TABLES;
+
+CREATE TABLE `ClassTime` (
+  `course_id` varchar(25) NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `day_of_week` char(3) NOT NULL,
+  PRIMARY KEY (course_id, start_time, end_time, day_of_week),
+  FOREIGN KEY (course_id) REFERENCES Course (course_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `ClassTime` WRITE;
+INSERT INTO `ClassTime` VALUES ("COMP3278", "10:30", "12:30", "FRI");
+UNLOCK TABLES;
+
+CREATE TABLE `CourseNote` (
+  `course_id` varchar(25) NOT NULL,
+  `note_link` varchar(100) NOT NULL,
+  PRIMARY KEY (course_id, note_link),
+  FOREIGN KEY (course_id) REFERENCES Course (course_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `CourseNote` WRITE;
+INSERT INTO `CourseNote` VALUES ("COMP3278", "lecture1_note.com");
+UNLOCK TABLES;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
